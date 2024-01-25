aeef6d404442ae43537ec701e9dea58b50d79295
Atilio Quintero <atilioquintero@gmail.com>
2024-01-25 01:52:48 -0500

    Se añaden comentarios y metodo para promedio.

--- src/gastos_dias.php
+++ src/gastos_dias.php
@@ -24,52 +24,132 @@
  */
 class GastosDias
 {
-	private DateTime $_dia;
-	private array $_montos = [];
+    /**
+     * El dia que contiene montos asociados. 
+     *
+     * @var DateTime
+     */
+    private DateTime $_dia;
+    
+    /**
+     * Lista con todos los gastos que hay en un dia.
+     *
+     * @var array
+     */
+    private array $_montos = [];
 
-	public function __construct(DateTime $dia, float $monto = 0) {
-	$this->_dia = $dia;
-	if ($monto != 0) {
-		$this->_montos[] = $monto;
-	}
-	}
+    /**
+     * Constructor de la clase
+     *
+     * @param DateTime $dia   El dia que tiene un gasto asociado.
+     * @param integer  $monto Uno de los montos que ocurrio en ese día.
+     */
+    public function __construct(DateTime $dia, float $monto = 0)
+    {
+        $this->_dia = $dia;
+        if ($monto != 0) {
+            $this->_montos[] = $monto;
+        }
+    }
 
-	public function agregarMonto(float $monto) {
-		if ($monto != 0) {
-			$this->_montos[] = $monto;
-		}		
-	}
+    /**
+     * Asocia un monto al listado de montos del día. 
+     *
+     * @param float $monto El valor del gasto que ocurrio ese día.
+     * 
+     * @return void 
+     */
+    public function agregarMonto(float $monto)
+    {
+        if ($monto != 0) {
+            $this->_montos[] = $monto;
+        }        
+    }
 
-	public function getObjetoDia() {
-		return $this->_dia;
-	}
+    /**
+     * Devuelve el dia como objeto de php.
+     *
+     * @return DateTime Objeto PHP con la información del día.
+     */
+    public function getObjetoDia()
+    {
+        return $this->_dia;
+    }
 
-	public function sumarMontos() {
-		return array_reduce($this->_montos, function($carry, $item) {
-			$carry += $item;
-			return $carry;
-		}, 0);
-	}
+    /**
+     * Devuelve la suma de todos los montos que ocurrieron en el día.
+     *
+     * @return float Sumatoria de los montos.
+     */
+    public function sumarMontos()
+    {
+        return array_reduce(
+            $this->_montos, function ($carry, $item) {
+                $carry += $item;
+                return $carry;
+            }, 0
+        );
+    }
 
-	public function getFecha() {
-		return $this->_dia->format("Ymd");
-	}
+    /**
+     * Devuelve el promedio de todos los montos asociados al día. 
+     *
+     * @return float Promedio de los montos para el día. 
+     */
+    public function montosPromedios()
+    {
+        return $this->sumarMontos() / count($this->_montos);
+    }
 
-	public function getAnnoMes() {
-		return $this->_dia->format("Ym");
-	}
+    /**
+     * Devuelve la fecha del día como texto en formato YYYYMMDD.
+     *
+     * @return string La fecha completa en texto. 
+     */
+    public function getFecha()
+    {
+        return $this->_dia->format("Ymd");
+    }
+
+    /**
+     * Devuelve el mes y año de la fecha en formato YYYYMM
+     *
+     * @return string El mes y el año como texto. 
+     */
+    public function getAnnoMes()
+    {
+        return $this->_dia->format("Ym");
+    }
 
 
-	public function getAnno() {
-		return $this->_dia->format("Y");
-	}
+    /**
+     * Devuelve el año de la fecha en formato YYYY.
+     *
+     * @return string El año como texto.
+     */
+    public function getAnno()
+    {
+        return $this->_dia->format("Y");
+    }
 
-	public function getDiaMes() {
-		return $this->_dia->format("d");
-	}
-	
-	public function getDiaAnno() {
-		return $this->_dia->format("z");
-	}
-	
+    /**
+     * Devuelve el día del mes de la fecha en formato DD.
+     *
+     * @return string El dia del mes como texto.
+     */
+    public function getDiaMes()
+    {
+        return $this->_dia->format("d");
+    }
+    
+    /**
+     * Devuelve el día del año de la fecha en formato DD.
+     *
+     * @return string El dia del año como texto.
+     */ 
+    public function getDiaAnno()
+    {
+        return $this->_dia->format("z");
+    }
+    
 }
